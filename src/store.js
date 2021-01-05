a8202ffc3f1cda150fdf658c2f596754cb76a7b6
Jonathan Graziani <jpgraziani@gmail.com>
2021-01-05 14:44:56 -0700

    removed and refactored logic from app

---
+++ src/store.js
@@ -0,0 +1,19 @@
+const bookmarks = [
+  { id: 11,
+    title: 'Medium',
+    url: 'https://medium.com/',
+    description: 'The best ideas can change who we are',
+    rating: 5 },
+  { id: 23,
+    title: 'Google',
+    url: 'https://www.google.com',
+    description: 'Where we find everything else',
+    rating: 2 },
+  { id: 316,
+    title: 'NYT',
+    url: 'https://www.nytimes.com/',
+    description: 'The New York Times from more than 150 countries around the world',
+    rating: 5 },
+]
+
+module.exports = { bookmarks }
